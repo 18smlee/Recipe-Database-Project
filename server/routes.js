8ad68483659b8625ea336516182d802625784890
kuchaudhari <chaudhari.kunaal@gmail.com>
2021-12-14 03:13:10 -0500

    Implement user detail page

--- server/routes.js
+++ server/routes.js
@@ -664,7 +664,7 @@ async function search_users_by_reviews(req, res) {
 // Route 10
 async function get_all_recipes_from_user(req, res) {
     const pageSize = req.query.pagesize ? req.query.pagesize : 10;
-    const userId = req.params.user_id;
+    const userId = req.params.userId;
 
     recipeSearchQuery = `
     SELECT *
@@ -691,7 +691,7 @@ async function get_all_recipes_from_user(req, res) {
 // Route 11
 async function get_all_reviews_from_user(req, res) {
     const pageSize = req.query.pagesize ? req.query.pagesize : 10;
-    const userId = req.params.user_id;
+    const userId = req.params.userId;
 
     reviewSearchQuery = `
     SELECT *
@@ -710,6 +710,7 @@ async function get_all_reviews_from_user(req, res) {
         if (error) {
             res.json({ results: [] })
         } else if (results) {
+            console.log(results)
             res.json({ results: results })
         }
         
@@ -779,7 +780,6 @@ async function get_all_users(req, res) {
         LIMIT ${pageSize} OFFSET ${pageSize * req.query.page};`
     }
 
-    console.log(userQuery);
 
     connection.query(userQuery, function (error, results, fields) {
         if (error) {
@@ -792,7 +792,8 @@ async function get_all_users(req, res) {
 }
 
 async function get_user_by_id(req, res) {
-    const userId = req.query.userId ? req.query.userId : 1;
+    const userId = req.params.userid ? req.params.userid : 1;
+    console.log(userId)
 
     var userQuery = `SELECT *
     FROM Users
