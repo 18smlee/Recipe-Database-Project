3ec9a01cc329ad5d9425d6cc5a578aedff7d3197
Sam Lee <smlee18@sas.upenn.edu>
2021-12-13 20:06:40 -0500

    polished chopped layout

--- client/src/components/ChoppedModal.js
+++ client/src/components/ChoppedModal.js
@@ -1,6 +1,15 @@
 
 import React from "react";
 import { getChoppedEpisodeIngredients, getChoppedSimilarRecipes } from '../fetcher';
+import {
+    Container,
+    Row, 
+    Col,
+    Button,
+    Badge
+} from "react-bootstrap"
+
+import person_icon from  '../images/logo.png';
 
 class ChoppedModal extends React.Component {
 
@@ -44,19 +53,48 @@ class ChoppedModal extends React.Component {
         } else {
             return (
                 <div className="card" style={{ margin: "20px", padding: "20px", alignItems: "center", justifyContent: "center", left: 0, top: 0, right: 0, bottom: 0, width: "600px", height:"40%", display: "flex" }} onClick={this.props.handleClose}>
-                  <div className="modal-main" style={{ margin: "20px", width: "500px" }} onClick={e => e.stopPropagation()}>
-                      <h4>{this.props.seriesEpisode} : {this.props.choppedEpisode.episode_name} </h4>
-                      <h5>{this.props.choppedEpisode.air_date}</h5>
+                  <div className="modal-main" style={{ margin: "10px", width: "500px" }} onClick={e => e.stopPropagation()}>
+                  <h2>{this.props.choppedEpisode.episode_name} </h2>
+                  <div style={{marginLeft:"20px"}}>
+                    <Badge bg="info">Ep. {this.props.seriesEpisode}</Badge>{' '}
+                    <Badge bg="light" text="dark">Aired on {this.props.choppedEpisode.air_date}</Badge>{' '}
+                  </div>
                       <div>
-                        <h6>Judges</h6>
-                        <ol>
-                          <li key={1}>{this.props.choppedEpisode.Judge1}</li>
-                          <li key={2}>{this.props.choppedEpisode.Judge2}</li>
-                          <li key={3}>{this.props.choppedEpisode.Judge3}</li>
-                        </ol>
+                      
+                        <h6 class="text-center" style={{margin: "25px"}}>Judges</h6>
+                        <Container>
+                        <Row>
+                            <Col>
+                            <img className="d-block mx-auto" src="https://raw.githubusercontent.com/18smlee/Recipe-Database-Project/sam_dev/client/src/images/person_icon.png" height="50" width="50"/>
+                            </Col>
+                            <Col>
+                            <img className="d-block mx-auto" src="https://raw.githubusercontent.com/18smlee/Recipe-Database-Project/sam_dev/client/src/images/person_icon.png" height="50" width="50"/>
+                            </Col>
+                            <Col>
+                            <img className="d-block mx-auto" src="https://raw.githubusercontent.com/18smlee/Recipe-Database-Project/sam_dev/client/src/images/person_icon.png" height="50" width="50"/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <p class="text-center">
+                                    {this.props.choppedEpisode.Judge1}
+                                </p>
+                            </Col>
+                            <Col>
+                                <p class="text-center">
+                                    {this.props.choppedEpisode.Judge2}
+                                </p>
+                            </Col>
+                            <Col>
+                                <p class="text-center">
+                                    {this.props.choppedEpisode.Judge3}
+                                </p>
+                            </Col>
+                        </Row>
+                        </Container>
                       </div>
                       <div>
-                        <h6>Ingredients</h6>
+                        <h6 style={{marginTop: "20px"}}>Ingredients</h6>
                         <ul>
                          {this.state.episodeIngredients.map((ingr, ind) => {
                              return(<li key={ind}>{ingr}</li>);
@@ -68,21 +106,23 @@ class ChoppedModal extends React.Component {
                         (         
                             <div>
                                 <h6>Similar Recipes</h6>
-                                <ul>
                                     {this.state.similarRecipes.map((rec) => {
                                         var url = "/recipes/" + rec.id
-                                        return(<li key={rec.id}><a href={url}>{rec.name}</a></li>);
+                                        return(
+                                            <Badge style={{margin: "10px"}} bg="light" key={rec.id} >
+                                                <a href={url}>{rec.name}</a>
+                                            </Badge>
+                                        );
                                     })}
-                                </ul>
                             </div>               
                         ) 
                         : 
                         (<></>)
                         }
                       </div>
-                      <button type="button" onClick={this.props.handleClose}>
+                      <Button style={{marginTop: "20px"}} variant="secondary" onClick={this.props.handleClose}>
                           Close
-                      </button>
+                      </Button>
                   </div>
                 </div>
               );
